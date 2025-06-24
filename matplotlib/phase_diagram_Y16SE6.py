b92a7836625a835ecd8687d9700d5674bbea599e
Mathieu Leocmach <mathieu.leocmach@univ-lyon1.fr>
2025-06-24 13:22:42 +0200

    function plot_phase_diagram(ax, Y, SE).

--- matplotlib/phase_diagram_Y16SE6.py
+++ matplotlib/phase_diagram_Y16SE6.py
@@ -1,21 +1,33 @@
 import numpy as np
+from scipy import constants
 from matplotlib import pyplot as plt
 
-fig, ax = plt.subplots(1,1, figsize=(3.375,3),layout='constrained')
 
-Cov = 1015 #µM
 
-#Sticky ends
-C_SE, T = np.loadtxt('../simulations/mfold_SE6_concentration.tsv', skiprows=1, usecols=[0,3], unpack=True)
-plt.plot(C_SE / 1.5 / Cov, T, label=r'$T_\mathrm{SE}$')
+def plot_phase_diagram(ax, Y=16, SE=6):
+    """plot the phase diagram (T, phi) on the matplotlib axis ax.
+    Y is the arm length in base pairs
+    SE is the sticky end length in base pairs"""
+    
+    #overlap concentration
+    R = 1 + 0.332 * (Y+SE/2) #nm
+    Cov = 1/(4/3*np.pi*(R*1e-9)**3*1e3)/constants.Avogadro*1e6 #µM
 
-C_NS, T = np.loadtxt('../simulations/melting_Y16SE0_concentrations.tsv', skiprows=1, usecols=[0,1], unpack=True)
-plt.plot(C_NS / Cov, T, label=r'$T_\mathrm{NS}$')
+    #Sticky ends
+    C_SE, T = np.loadtxt(f'../simulations/mfold_SE{SE}_concentration.tsv', skiprows=1, usecols=[0,3], unpack=True)
+    ax.plot(C_SE / 1.5 / Cov, T, label=r'$T_\mathrm{SE}$')
+    
+    #Nanostars
+    C_NS, T = np.loadtxt(f'../simulations/melting_Y{Y}SE0_concentrations.tsv', skiprows=1, usecols=[0,1], unpack=True)
+    ax.plot(C_NS / Cov, T, label=r'$T_\mathrm{NS}$')
 
-ax.set_xlabel(r'$\phi_\mathrm{NS}=C_\mathrm{NS}/C_\mathrm{ov}$')
-ax.set_ylabel(r'$T$ (°C)')
-ax.set_xlim(0,1.2)
-ax.set_ylim(0, 100)
+    ax.set_xlabel(r'$\phi_\mathrm{NS}=C_\mathrm{NS}/C_\mathrm{ov}$')
+    ax.set_ylabel(r'$T$ (°C)')
+    ax.set_xlim(0,1.2)
+    ax.set_ylim(0, 100)
 
-plt.show()
+if __name__ == "__main__":
+    fig, ax = plt.subplots(1,1, figsize=(3.375,3),layout='constrained')
+    plot_phase_diagram(ax)
+    plt.show()
 
