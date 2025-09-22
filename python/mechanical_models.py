a14e3b242413aa5cedca90b4772691645d3b88f4
Mathieu Leocmach <mathieu.leocmach@univ-lyon1.fr>
2025-09-22 16:09:42 +0200

    Object oriented Diagram that overload operator+ for series and operator* for parallel

--- python/mechanical_models.py
+++ python/mechanical_models.py
@@ -4,6 +4,147 @@ from scipy import constants as const
 from scipy.special import gamma
 import mpmath
 
+class Diagram(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @property
+    @abstractmethod
+    def width(self):
+        pass
+
+    @property
+    @abstractmethod
+    def height(self):
+        pass
+
+    @staticmethod
+    def str2array(s):
+        lines_lengths =  list(map(len, s.splitlines()))
+        maxl = max(lines_lengths)
+        assert lines_lengths[-2] == maxl, """connection must be the longest line"""
+        return np.array([
+            [
+                c
+                for c in line.ljust(maxl)
+            ]
+            for line in s.splitlines()
+        ])
+
+    def __add__(self, other):
+        """self and other in series"""
+        return SeriesDiagram(self, other)
+
+    def __mul__(self, other):
+        """self and other in parallel"""
+        return ParallelDiagram(self, other)
+
+class DiagramLeaf(Diagram):
+    """A single component in the diagram"""
+
+    def __str__(self):
+        return "\n".join(["".join(line) for line in self.array])
+
+    @property
+    def width(self):
+        return self.array.shape[1]
+
+    @property
+    def height(self):
+        return self.array.shape[0]
+
+class Spring(DiagramLeaf):
+    """A spring of modulus G."""
+    def __init__(self, G):
+        self.array = Diagram.str2array(f"""
+
+____╱╲  ╱╲  ╱╲  ___{'_'*len(G)}
+      ╲╱  ╲╱  ╲╱ {G}
+""")
+
+class Dashpot(DiagramLeaf):
+    """A dashpot of viscosity eta."""
+    def __init__(self, eta):
+        self.array = Diagram.str2array(f"""
+    ___
+_____| |____{'_'*len(eta)}
+    _|_| {eta}
+""")
+
+class Springpot(DiagramLeaf):
+    """A springpot of exponent alpha and quasiproperty V."""
+    def __init__(self, V, alpha):
+        self.array = Diagram.str2array(f"""
+
+____╱╲____{'_'*(len(V)+len(alpha))}
+    ╲╱ {V}, {alpha}
+""")
+
+class SeriesDiagram(Diagram):
+    """Two or more elements in series"""
+    def __init__(self, *args):
+        assert(len(args)>1), f"Only {len(args)} elements were provided."
+        self.children = []
+        for child in args:
+            if isinstance(child, SeriesDiagram):
+                self.children += child.children
+            else:
+                self.children.append(child)
+    @property
+    def width(self):
+        return sum(child.width for child in self.children)
+
+    @property
+    def height(self):
+        return max(child.height for child in self.children)
+
+    def __str__(self):
+        ret = ['' for l in range(self.height)]
+        for child in self.children:
+            for l, line in enumerate(f'{child}'.splitlines()[::-1]):
+                ret[len(ret)-l-1] += line
+        return "\n".join(ret)
+
+class ParallelDiagram(Diagram):
+    """Two or more elements in parallel"""
+    def __init__(self, *args):
+        assert(len(args)>1), f"Only {len(args)} elements were provided."
+        self.children = []
+        for child in args:
+            if isinstance(child, ParallelDiagram):
+                self.children += child.children
+            else:
+                self.children.append(child)
+    @property
+    def width(self):
+        return max(child.width for child in self.children)+8
+
+    @property
+    def height(self):
+        return sum(child.height for child in self.children)
+
+    def __str__(self):
+        maxL = self.width-8
+        ret = []
+        lines = f'{self.children[0]}'.splitlines()
+        for line in lines[:-2]:
+            ret.append(line.center(maxL+8))
+        ret.append(' '*4 + lines[-2].center(maxL, '_')+' '*4)
+        ret.append('   |'+lines[-1].center(maxL)+'|   ')
+        for child in self.children[1:-1]:
+            lines = f'{self.child}'.splitlines()
+            for line in lines[:-2]:
+                ret.append('   |'+line.center(maxL)+'|   ')
+            ret.append('   |'+lines[-2].center(maxL, '_')+'|   ')
+            ret.append('   |'+lines[-1].center(maxL)+'|   ')
+        lines = f'{self.children[-1]}'.splitlines()
+        for line in lines[:-2]:
+            ret.append('   |'+line.center(maxL)+'|   ')
+        ret.append('___|'+lines[-2].center(maxL, '_')+'|___')
+        ret.append(lines[-1].center(maxL+8))
+        return '\n'.join(ret)
+
 def text_spring(G):
     return f"""
 
