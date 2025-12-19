08a51857e56575c7131f52f01ff5c0b8e89dea90
haj-aj <ajiyelhajar@gmail.com>
2025-12-19 16:46:00 +0900

    script for paper figure 2

--- python/Y6_TTS.py
+++ python/Y6_TTS.py
@@ -12,6 +12,13 @@ import os
 from scipy.interpolate import interp1d
 from scipy.optimize import root_scalar, curve_fit, least_squares, leastsq
 from scipy.special import gamma
+import uncertainties
+import matplotlib as mpl
+from matplotlib.ticker import LogLocator
+import matplotlib.gridspec as gridspec
+
+mpl.rcdefaults()
+plt.style.use(['plt-style-2.mplstyle', 'tableau-colorblind10'])
 
 π = np.pi
 def fmmGp(ω, V, G, α, β):
@@ -80,15 +87,26 @@ os.chdir(r"C:\Users\ajiye\Documents\Rheometre\20250122")
 
 # figures setup
 
-fig, axs = plt.subplots(3, 2, figsize=(7.3*1.2,7.3), sharex='col', layout='constrained')
+# fig, axs = plt.subplots(3, 2, figsize=(89.7/25.4, 89.7/25.4*1.2), sharex='col', layout='constrained')#, gridspec_kw={'bottom': 0.20})
+fig = plt.figure(
+    figsize=(89.7/25.4, 89.7/25.4 * 1.4),
+    layout="constrained"
+)
+
+gs = gridspec.GridSpec(4, 2, figure=fig, height_ratios=[1, 1, 1, 0.3],wspace=0.001)
+
+axs = np.empty((3,2), dtype=object)
+for i in range(3):
+    for j in range(2):
+        axs[i,j] = fig.add_subplot(gs[i, j])
+
 # fig1, ax1 = plt.subplots(1, 1, figsize=(3,2), layout='constrained')
 # fig2, ax2 = plt.subplots(1, 1, figsize=(3,2), layout='constrained')
 ax2 = axs[0,1].inset_axes([0.6, 0.5, 0.35, 0.4])
 ax1 = axs[1,1].inset_axes([0.6, 0.15, 0.35, 0.4])
 
-for ax in axs.ravel():
-    ax.set_xscale('log')
-    ax.set_yscale('log')
+
+    
 axs[2,0].set_xlabel('ω (rad/s)')
 axs[2,1].set_xlabel('ωτ')
 axs[0,0].set_ylabel('tan δ')
@@ -211,7 +229,10 @@ for i in n:
     # axs[1,1].plot(omega*tau, fmmGp(omega, G*fmmVGratio(tau, alpha, beta), G, alpha, beta)/G, color=line.get_color())
     # axs[2,1].plot(omega*tau, fmmGpp(omega, G*fmmVGratio(tau, alpha, beta), G, alpha, beta)/G, color=line.get_color())
     axs[2,1].plot(omega*tau, Gpp/G*tau**beta0, marker='v', linestyle='None', color=line.get_color())
+
 axs[0,1].plot(omega*tauM, mmtandelta(omega, 1, tauM), linestyle='--', color='black')
+axs[1,1].plot(np.logspace(-2,4), fmmGp(np.logspace(-2,4), 1, 1, alpha0, beta0), linestyle='--', color='black')
+axs[2,1].plot(np.logspace(-2,4), fmmGpp(np.logspace(-2,4), 1, 1, alpha0, beta0), linestyle='--', color='black')
 
 ax1.plot(Ts, Gs/taus**beta0, marker='d', label=r'$\mathbb{G}/\tau^\beta$')
 ax2.plot(Ts, taus, marker='d', label=r'$\tau$')
@@ -223,19 +244,49 @@ axs[2,0].set_ylim(1e1, 7e2)
 axs[0,1].set_ylim(1e-2, 3e1)
 axs[1,1].set_ylim(1e-4, 3e0)
 axs[2,1].set_ylim(1e-2, 1e0)
-axs[0,0].text(2e-4, 3e1, 'A', color='black', fontsize=14)
-axs[1,0].text(2e-4, 3e3, 'B', color='black', fontsize=14)
-axs[2,0].text(2e-4, 7e2, 'C', color='black', fontsize=14)
-axs[0,1].text(3e-4, 3e1, 'D', color='black', fontsize=14)
-axs[1,1].text(3e-4, 3e0, 'E', color='black', fontsize=14)
-axs[2,1].text(3e-4, 1e0, 'F', color='black', fontsize=14)
-axs[1,0].legend()
+axs[0,0].text(8e-5, 3e1, 'A', color='black')
+axs[1,0].text(8e-5, 3e3, 'B', color='black')
+axs[2,0].text(8e-5, 7e2, 'C', color='black')
+axs[0,1].text(2e-5, 3e1, 'D', color='black')
+axs[1,1].text(2e-5, 3e0, 'E', color='black')
+axs[2,1].text(2e-5, 1e0, 'F', color='black')
+# axs[1,0].legend()
+
 # ax1.legend(fontsize=8, loc='center right')
 ax2.set_yscale('log')
 # ax1.set_ylim(900, 1500)
 ax1.xaxis.tick_top()
 # ax2.xaxis.set_label_position('top')
+for ax in [ax1, ax2]:
+    ax.tick_params(axis='both', which='major', labelsize=6)   
+    ax.tick_params(axis='both', which='minor', labelsize=5)   
+    ax.xaxis.label.set_size(7)                             
+    ax.yaxis.label.set_size(7) 
 
-figname = r"C:\Users\ajiye\Documents\Redaction\paper dna gels\figures\Y6-TTS-22012025-insets-onealphabeta"
+for ax in axs.ravel():
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.xaxis.set_major_locator(LogLocator(base=10, numticks=20))
+    ax.xaxis.set_minor_locator(LogLocator(base=10, subs='auto'))
+    ax.tick_params(axis='x', which='minor', length=2)
+
+# fig.set_constrained_layout(False)
+fig.get_layout_engine().set(h_pad=0.05, w_pad=0.001)
+
+legend_ax = fig.add_subplot(gs[3, :])
+# legend_ax = fig.add_axes([0, -0.01, 1, 0.15])  
+legend_ax.axis('off')
+handles, labels = axs[1,0].get_legend_handles_labels()
+
+legend_ax.legend(
+    handles, labels,
+    loc='center',
+    # bbox_to_anchor=(0.5, -0.02),   # adjust spacing below
+    ncol=3,              # horizontal legend
+    frameon=True,
+    fontsize=7,                    # adjust for journal size
+)
+
+figname = r"C:\Users\ajiye\Documents\Redaction\paper dna gels\figures\Y6-TTS-22012025-colourblind"
 for ex in [".png", ".pdf"]:
     fig.savefig(figname+ex, bbox_inches='tight', pad_inches=0.2)
